99d06b4b21f47e6001f8bf77dca774c2ce88ed87
JavierJ <ingenierodesoftware73@gmail.com>
2024-12-24 10:42:52 -0500

    Herencia

--- Lesson08/0bjects.js
+++ Lesson08/0bjects.js
@@ -15,6 +15,20 @@ class Person {
   }
 }
 
+class Employer extends Person{
+  constructor(name, lastname, cargo){
+    super(name, lastname);
+    this._cargo = cargo;
+  }
+
+  get cargo(){
+    return this._cargo;
+  }
+
+  set cargo(cargo){
+    this._cargo = cargo;
+  }
+}
 let user1 = new Person('Juan', 'Jara')
 console.log(user1)
 let user2 = new Person('Carlos', 'Caracol');
@@ -22,3 +36,6 @@ console.log(user2)
 console.log(user1.name)
 user1.name = 'Oscar';
 console.log(user1.name)  
+
+let employer1 = new Employer('Armando', 'Vendedor', 'Ventas');
+console.log(employer1)
