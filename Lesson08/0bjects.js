064faad34116545fd84721bed5fa7f177077382f
JavierJ <ingenierodesoftware73@gmail.com>
2024-12-24 10:18:11 -0500

    gets y sets

--- Lesson08/0bjects.js
+++ Lesson08/0bjects.js
@@ -2,8 +2,16 @@
 
 class Person {
   constructor(name, lastname){
-    this.name = name;
-    this.lastname = lastname;
+    this._name = name;
+    this._lastname = lastname;
+  }
+
+  get name(){
+    return this._name;
+  }
+
+  set name(name){
+    this._name = name
   }
 }
 
@@ -11,3 +19,6 @@ let user1 = new Person('Juan', 'Jara')
 console.log(user1)
 let user2 = new Person('Carlos', 'Caracol');
 console.log(user2)
+console.log(user1.name)
+user1.name = 'Oscar';
+console.log(user1.name)  
